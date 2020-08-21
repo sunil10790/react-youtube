9b55e7cfe6fa2315595defe958afbd9d543c97a8
Sunil Kumar C <sunil10790@outlook.com>
2020-08-21 08:42:18 +0530

    Add Input

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,7 +2,16 @@ import React from "react";
 
 class SearchBar extends React.Component {
   render() {
-    return <div>Search Bar Component</div>;
+    return (
+      <div className="search-bar ui segment">
+        <form class="ui form">
+          <div class="field">
+            <label>Video Search</label>
+            <input type="text" name="video-search" placeholder="Search" />
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
