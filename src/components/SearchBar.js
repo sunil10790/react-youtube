ea3bc545e145f5f79cb52125c028f5a4d7ba0ea6
Sunil Kumar C <sunil10790@outlook.com>
2020-08-21 08:44:06 +0530

    Add Event Handler for Search Input

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,23 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  state = { searchTerm: "" };
+
+  onSearchInputChange = () => {};
+
   render() {
     return (
       <div className="search-bar ui segment">
         <form class="ui form">
           <div class="field">
             <label>Video Search</label>
-            <input type="text" name="video-search" placeholder="Search" />
+            <input
+              type="text"
+              value={this.state.searchTerm}
+              onChange={this.onSearchInputChange}
+              name="video-search"
+              placeholder="Search"
+            />
           </div>
         </form>
       </div>
