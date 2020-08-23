009963e45335243b53c5d9f5b6f591be7a548de3
Sunil Kumar C <sunil10790@outlook.com>
2020-08-23 14:10:41 +0530

    Configure YOUTUBE API

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,12 @@
+import axios from "axios";
+
 const KEY = "AIzaSyDD5-kk1JaZmPA226kgsz1otdWkP3r_sNg";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    key: KEY,
+  },
+});
