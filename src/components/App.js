5f5e0c99a8467db7774abe6eaf1765b03e79e1b9
Sunil Kumar C <sunil10790@outlook.com>
2020-08-27 00:33:18 +0530

    Pass selectedVideo as prop to VideoDetail Component

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,13 @@ import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
 import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 class App extends React.Component {
-  state = { videoList: [] };
+  state = {
+    videoList: [],
+    selectedVideo: null,
+  };
 
   onSearchTermSubmit = async (term) => {
     const response = await youtube.get("/search", {
@@ -17,13 +21,14 @@ class App extends React.Component {
   };
 
   onVideoSelect = (video) => {
-    console.log("Inside App Component", video);
+    this.setState({ selectedVideo: video });
   };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onSearchTermSubmit={this.onSearchTermSubmit} />
+        <VideoDetail video={this.state.selectedVideo} />
         <VideoList
           onVideoSelect={this.onVideoSelect}
           videos={this.state.videoList}
